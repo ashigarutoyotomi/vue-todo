1248eeefbd5711dbb677d27c5a65e3e7ae874b3b
toyotomi nashimoto <toyotominashimoto@gmail.com>
2023-10-28 15:50:43 +0600

    push

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%">
+    <list msg="Hello Vue in CodeSandbox!"/>
+  </div>
+</template>
+
+<script>
+import list from "./components/list";
+
+export default {
+  components: {
+    list
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <p v-if="todoList.length >0">
+    todo els
+    <ul>
+      <li v-for="item in todoList">{{item}}</li>
+      </ul>
+    </p>
+    
+    <p v-else>no items</p>
+    <input v-model="field" type="text">
+    <button @click="add"> add</button>
+    <button @click="remove"> delete</button></div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {todoList:[],
+    field:""};
+  },
+  mounted(){
+    if(localStorage.getItem('todoList')==undefined){
+      this.todoList =[]
+    }else{
+      this.todoList = JSON.parse(localStorage.getItem('todoList'))
+    }
+  },
+  methods:{
+    add:function (event){
+      this.todoList.push(this.field)
+      localStorage.setItem('todoList',JSON.encode(this.todoList))
+      this.field=null
+    },
+    remove :function(){
+      this.todoList.pop()
+    }
+  }
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
